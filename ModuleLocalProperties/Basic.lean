b83fa26d879767f923c8b18b925719bc63cf5610
su00000 <ssh@stu.pku.edu.cn>
2024-09-26 19:20:59 +0800

    Update Basic.lean

--- ModuleLocalProperties/Basic.lean
+++ ModuleLocalProperties/Basic.lean
@@ -26,42 +26,26 @@ Subsingleton M :=  subsingleton_of_forall_eq 0 fun x ↦ eq_zero_of_localization
 lemma submodule_le_of_localization {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M] (N : Submodule R M) (P : Submodule R M) (h : ∀ (J : Ideal R) (hJ : J.IsMaximal), Submodule.localized J.primeCompl N ≤ Submodule.localized J.primeCompl P) : N ≤ P := by
   by_contra nle
   obtain ⟨n, hn, hp⟩ := Set.not_subset.mp nle
-  set I : Ideal R := {
-    carrier := {x : R | x • n ∈ P}
-    add_mem' := by
-      intro a b ha hb
-      exact (Set.mem_setOf_eq) ▸ (add_smul a b n).symm ▸ (Submodule.add_mem P ha hb)
-    zero_mem' := by simp only [Set.mem_setOf_eq, zero_smul, Submodule.zero_mem]
-    smul_mem' := by
-      intro c x hx
-      rw [smul_eq_mul, Set.mem_setOf_eq, ← smul_smul]
-      exact smul_mem P c hx
-  }
-  have : I ≠ ⊤ := by
-    apply (Ideal.ne_top_iff_one _).mpr
-    by_contra! H
-    unfold_let at H
-    rw [mem_mk, AddSubmonoid.mem_mk, AddSubsemigroup.mem_mk, Set.mem_setOf_eq, one_smul] at H
-    exact hp H
-  obtain ⟨m ,maxm, lem⟩ := Ideal.exists_le_maximal _ this
-  obtain H := h m maxm
+  set I := P.colon (Submodule.span R {n})
+  obtain ⟨m ,maxm, lem⟩ := Ideal.exists_le_maximal _ ((Ideal.ne_top_iff_one _).mpr
+    fun H ↦ (one_smul R n ▸ hp) (Submodule.mem_colon_singleton.mp H))
   have mem : (mkLinearMap m.primeCompl M) n ∈ localized m.primeCompl N := by
     simp only [mkLinearMap_apply, Submodule.mem_localized']
     exact ⟨n, hn, 1, by rw [mk'_one, mkLinearMap_apply]⟩
-  apply H at mem
+  apply (h m maxm) at mem
   simp only [mkLinearMap_apply, Submodule.mem_localized', mk_eq_mk'] at mem
   obtain ⟨p, inp, s, hs⟩ := mem
   obtain ⟨s', eq⟩ := (IsLocalizedModule.mk'_eq_mk'_iff _ p n s 1).mp hs
   simp only [one_smul, smul_smul] at eq
-  have h1 : (s' * s).1 ∈ I := by
+  have h1 : (s'.1 * s.1) ∈ I := by
     unfold_let
     have : (s'.1 * s.1) • n = (s' * s) • n := rfl
-    rw [Submonoid.coe_mul, mem_mk, AddSubmonoid.mem_mk, AddSubsemigroup.mem_mk, Set.mem_setOf_eq, this, eq]
+    rw [Submodule.mem_colon_singleton, this, eq]
     exact smul_mem P (m.primeCompl.subtype s') inp
   exact (s' * s).2 (lem h1)
 
 lemma submodule_eq_of_localization {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M]
-(N : Submodule R M) (P : Submodule R M) (h : ∀ (J : Ideal R) (hJ : J.IsMaximal),
+(N : Submodule R M) (P : Submodule R M) (h : ∀ (J : Ideal R) (_ : J.IsMaximal),
 Submodule.localized J.primeCompl N = Submodule.localized J.primeCompl P) : N = P :=
   eq_of_le_of_le (submodule_le_of_localization _ _ (fun J hJ ↦ le_of_eq (h J hJ)))
   (submodule_le_of_localization _ _ (fun J hJ ↦ le_of_eq (h J hJ).symm))
