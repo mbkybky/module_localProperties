93e60025e72a9b8eb97e7f644e91dce95d0aaf80
syur2 <sif4delta0@mail.ustc.edu.cn>
2024-10-06 22:51:42 +0800

    Update LocalizedModule.lean

--- ModuleLocalProperties/MissingLemmas/LocalizedModule.lean
+++ ModuleLocalProperties/MissingLemmas/LocalizedModule.lean
@@ -5,11 +5,14 @@ Authors: Yi Song
 -/
 import Mathlib.Algebra.Module.Submodule.Localization
 
+import ModuleLocalProperties.Defs
+
 open Submodule IsLocalizedModule LocalizedModule Ideal IsLocalization TensorProduct
+
+section LiftOnLocalization
 
 variable {R : Type*} [CommRing R] (S : Submonoid R) {M N : Type*} [AddCommGroup M] [Module R M]
   [AddCommGroup N] [Module R N] [Module (Localization S) N] [IsScalarTower R (Localization S) N]
-
 
 def inv (s : S) : Module.End R N where
   toFun := fun n => (Localization.mk 1 s) • n
@@ -39,8 +42,51 @@ noncomputable def LiftOnLocalization' (f : M →ₗ[R] N) : LocalizedModule S M 
 noncomputable def LiftOnLocalization (f : M →ₗ[R] N) : LocalizedModule S M →ₗ[Localization S] N
   := LinearMap.extendScalarsOfIsLocalization S _ (LiftOnLocalization' _ f)
 
+end LiftOnLocalization
 
+section LocalizedMapSubmodule
 
+variable {R : Type*} [CommRing R] (S : Submonoid R) {M N : Type*}
+    [AddCommGroup M] [Module R M] [AddCommGroup N] [Module R N]
+
+noncomputable def LocalizedModule.map' :
+    (M →ₗ[R] N) →ₗ[R] (LocalizedModule S M) →ₗ[Localization S] (LocalizedModule S N) where
+  toFun := fun f => LinearMap.extendScalarsOfIsLocalization S _ <| LocalizedModule.map S f
+  map_add' := by
+    intro f g
+    ext x
+    dsimp
+    rw [map_add, LinearMap.add_apply]
+  map_smul' := by
+    intro r f
+    ext x
+    dsimp
+    rw [_root_.map_smul, LinearMap.smul_apply]
+
+def LocalizedMapSubmodule : Submodule (Localization S)
+    ((LocalizedModule S M) →ₗ[Localization S] (LocalizedModule S N)) where
+  carrier := {g | ∃ f ,∃ s : S, g = (Localization.mk 1 s) • (LocalizedModule.map' _ f)}
+  add_mem' := by
+    intro g1 g2 ⟨f1, s1, h1⟩ ⟨f2, s2, h2⟩
+    use s2 • f1 + s1 • f2, s1 * s2
+    rw [h1, h2]
+    simp only [ map_add, LinearMap.map_smul_of_tower, smul_add]
+    symm
+    nth_rw 1 [smul_comm,← smul_assoc]
+    simp
+    nth_rw 2 [smul_comm]
+
+    sorry
+  zero_mem' := by
+    use 0, 1
+    rw [Localization.mk_eq_monoidOf_mk', _root_.map_zero, smul_zero]
+  smul_mem' := by
+    intro r g ⟨f, s, h1⟩
+    haveI h2 : IsLocalization S (Localization S) := inferInstance
+    have := h2.surj r
+    sorry
+
+end LocalizedMapSubmodule
 /-
 example {R : Type*} (M N : Type*) [CommRing R] [AddCommGroup M] [Module R M] [AddCommGroup N]
     [Module R N] (S : Submonoid R) : IsLocalizedModule S (LocalizedModule.map S M N) := sorry
